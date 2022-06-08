68b04e2264f41d5e8a7e58fae1e8fe9d679fb1bd
Pavlo Koval <koval.pavlo03@gmail.com>
2022-06-08 17:00:55 +0300

    Instead of counting constants or registers i save it at the memmory

--- main.cpp
+++ main.cpp
@@ -3,12 +3,7 @@
 
 int main() {
     SHA256 a;
-    std::cout << "abc:          " << a.codeMsg("abc") << std::endl;
-    std::cout << "Hello:        " << a.codeMsg("Hello") << std::endl;
-
-    std::cout << "How are you?: " << a.codeMsg("How are you?") << std::endl;
-    std::cout << "How are yOu?: " << a.codeMsg("How are yOu?") << std::endl;
-    std::cout << "How Are you?: " << a.codeMsg("How Are you?") << std::endl;
+    std::cout << "Hello:        " << a.codeMsg("abc") << std::endl;
 
     return 0;
 }
